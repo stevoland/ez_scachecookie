db491c50d360d154cb112551bb26a85c6c18233c
stevoland <stevoland@gmail.com>
2010-03-08 19:58:05 +0100

    updated ezinfo

--- ezinfo.php
+++ ezinfo.php
@@ -6,6 +6,7 @@ class sCacheCookieInfo
         return array( 'Name' => "Cache Cookie",
                       'Version' => "0.1",
                       'Copyright' => "Copyright (C) 2010 <a href='http://www.stevoland.com' title='eZ Publish'>stevoland ltd</a>"
+                      'License'   => 'GNU General Public License v2.0',
                      );
     }
 }
